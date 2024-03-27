86f8d304caedab0c0f69b427553f0ad100fee5c6
JCGutierrezConcha <jc.gutierrez.concha@gmail.com>
2024-03-27 01:09:17 -0300

    Actualizar a Bootstrap 5.3.2

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,7 +25,7 @@ const validarFormulario = () => {
 }
 
 
-// Evento de cambio de la imagen del preview (al final del formulario)
+// Evento de cambio de la imagen del preview (al final del formulario).
 animalDom.addEventListener("change", async (event) => {
     const animalSelected = event.target.value;
     const imagenSrc = (await multimedia(animalSelected)).imagen;
@@ -66,10 +66,11 @@ const crearAnimal = async (animal) => {
     const contenedor = document.createElement("div");
     contenedor.innerHTML = cardAnimal;
     tablaAnimales.appendChild(contenedor);
-    const modalAnimal = crearModal(nuevoAnimal, index);
-    const contenedorModal = document.createElement("div");
-    contenedorModal.innerHTML = modalAnimal;
-    modalDom.appendChild(contenedorModal);
+    //const modalAnimal = crearModal(nuevoAnimal, index);
+    //const contenedorModal = document.createElement("div");
+    //contenedorModal.innerHTML = modalAnimal;
+    //modalDom.appendChild(contenedorModal);
+
 }
 
 // Función para limpiar el formulario.
@@ -80,11 +81,12 @@ const limpiarForm = (nombre, edad, comentarios) => {
     previewDom.style.backgroundImage = 'url("./assets/imgs/lion.svg")'
 }
 
-// Función de crea Card con imagen e icono de sonido
+// Función de crea Card con imagen e icono de sonido,
 const crearAnimalCard = (animal, index) => {
     return `<div id="${animal.nombre}${index}"class="card text-white bg-secondary m-3">
                 <p class="animal-name">${animal.nombre}</p>
-                <img type= button style="width: 10rem;" src="${animal.img}" class ="card-top" data-bs-toggle="modal" data-bs-target="#${animal.nombre}-${index}"/>
+                <button type="button" data-bs-toggle="modal" data-bs-target="#${animal.nombre}-${index}">
+                <img style="width: 10rem;" src="${animal.img}" class ="card-top"></button>
                 <div class="card-body p-1">                    
                  <img onclick="playSound('${index}')" class="p-1" height="30rem" src="./assets/imgs/audio.svg"/>
                  <audio id="animalAudio${index}">
@@ -94,7 +96,7 @@ const crearAnimalCard = (animal, index) => {
             </div>`
 };
 
-// Función para activar sonido de una nimal determinado,
+// Función para activar sonido de un determinado animal.
 const playSound = (animalId) => {
     let audioAnimal = document.getElementById('animalAudio' + animalId)
     audioAnimal.play()
@@ -103,30 +105,30 @@ const playSound = (animalId) => {
 window.playSound = playSound;
 
 
-// Funciónpara crear Modal con información de nombre, edad y comentarios de undeerminado animal.
+// Función para crear Modal con información de nombre, edad y comentarios de un determinado animal.
 const crearModal = (animal, index) => {
     return `<div class="modal fade" id="${animal.nombre}-${index}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered w-25 role=document">
-        <div class="modal-content bg-dark text-white">
-        <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">${animal.nombre}</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-            <img src="${animal.img}" class="img-fluid">
-            <hr>
-            <h5>Edad</h5>
-            <p>${animal.edad}</p>
-            <hr>
-            <h5>Comentarios</h5>
-            <p>${animal.comentarios}</p> 
-        </div>
-         <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-        </div>
-        </div>
-    </div>
-    </div>`
+                <div class="modal-dialog modal-dialog-centered w-25 role=document">
+                    <div class="modal-content bg-dark text-white">
+                        <div class="modal-header">
+                           <h5 class="modal-title" id="exampleModalLabel">${animal.nombre}</h5>
+                           <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                           <img src="${animal.img}" class="img-fluid">
+                           <hr>
+                           <h5>Edad</h5>
+                           <p>${animal.edad}</p>
+                           <hr>
+                          <h5>Comentarios</h5>
+                          <p>${animal.comentarios}</p> 
+                        </div>
+                        <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>`
 }
 
 // Evento que activa funciones al hacer click en boton para el registro de un animal.
